f77b44ec2030ee02ed38b153d89d9efc269d398b
SpRajib <sprajiblochansahoo@gmail.com>
2026-05-18 19:40:25 +0530

    popup

--- Test_codes/FileDownload_popup.py
+++ Test_codes/FileDownload_popup.py
@@ -1,16 +1,14 @@
-
 from selenium.webdriver import Chrome, ChromeOptions
 from time import sleep
 
 from selenium.webdriver.common.by import By
 
 o = ChromeOptions()
+o.add_experimental_option("prefs", {"safebrowsing.enabled": True})
 o.add_experimental_option("detach", True)
 
 driver = Chrome(options=o)
-driver.get("https://www.shine.com/")
+driver.get("https://www.python.org/downloads/")
 driver.maximize_window()
 
-driver.find_element(By.XPATH, "//a[text()='Register']").click()
-sleep(2)
-driver.find_element(By.XPATH, "//input[@type='file']").send_keys("/Users/rajib/Downloads/Rajib_Sahoo_IBM.pdf")
+driver.find_element(By.XPATH, "(//a[text()='Download Python 3.14.5'])[2]").click()
